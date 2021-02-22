d7e3d52f9c2bb6ac68017f40010b9c5211712640
Wilson Alves <paquitoyde@gmail.com>
2021-02-22 15:36:16 -0300

    Adicionando a mensagem Good Afternoon

--- TesteGitFinal/Program.cs
+++ TesteGitFinal/Program.cs
@@ -4,6 +4,7 @@ namespace TesteGitFinal {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Afternoon!");
             Console.ReadLine();
         }
     }
