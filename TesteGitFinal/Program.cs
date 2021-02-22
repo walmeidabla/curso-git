8657208b5ba4b48d10e82900ff138ecca17299c9
Wilson Alves <paquitoyde@gmail.com>
2021-02-22 15:29:47 -0300

    Projeto iniciado, mostrando a mensgem Hellow World

---
+++ TesteGitFinal/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TesteGitFinal {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
