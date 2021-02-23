c5ae21e9190d7ff4b14268bcd967198d47f061e5
Wilson Alves <alvesdealmeidawilson@gmail.com>
2021-02-22 22:27:28 -0300

    adicionando traços de separação

--- TesteGitFinal/Program.cs
+++ TesteGitFinal/Program.cs
@@ -3,11 +3,14 @@
 namespace TesteGitFinal {
     class Program {
         static void Main(string[] args) {
+
+            Console.WriteLine("-------------------------------");
             Console.WriteLine("Good Afternoon!");
             Console.WriteLine("Tenha um otimo dia");
             Console.WriteLine("Tenha um otimo dia");
             Console.WriteLine("Como vc está agora");
             Console.WriteLine("Não se esqueça de passar na padaria");
+            Console.WriteLine("-------------------------------");
             Console.ReadLine();
         }
     }
